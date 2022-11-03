62a119c67fd22f350a135fd373c8fb85ddff81b1
vbartalis <vbartalis@gmail.com>
2022-11-03 21:27:15 +0200

    add tetelek

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "vbartalis", // Usually your GitHub org/user name.
   projectName: "vbartalis.github.io", // Usually your repo name.
-  deploymentBranch: "deployment",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -37,15 +36,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/vbartalis/vbartalis.github.io/tree/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/vbartalis/vbartalis.github.io/tree/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
